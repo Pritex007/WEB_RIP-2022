810ff99e2dd939b896d36c17efa4ce0e8236a6e6
Pritex007 <93098673+Pritex007@users.noreply.github.com>
2022-09-26 14:08:32 +0300

    additional task lab1 done

--- bmstu_lab/views.py
+++ bmstu_lab/views.py
@@ -7,16 +7,48 @@ from django.shortcuts import render
 def GetCar(request, title):
     return render(request, 'car.html', {'data' : {
         'current_date': date.today(),
-        'title': title
+        'title': title,
+        'imagePath': TakeCarImagePath(title),
+        'description': TakeCarDescription(title)
     }})
 
 def GetCars(request):
     return render(request, 'cars.html', {'data' : {
         'current_date': date.today(),
         'orders': [
-            {'title': 'Ford', 'id': 1},
-            {'title': 'Tesla', 'id': 2},
-            {'title': 'BMW', 'id': 3},
-            {'title': 'Mercedes', 'id': 4},
+            {'title': 'Ford', 'id': 1, 'eng_type': 'преимущественно ДВС'},
+            {'title': 'Tesla', 'id': 2, 'eng_type': 'Электрический'},
+            {'title': 'BMW', 'id': 3, 'eng_type': 'преимущественно ДВС'},
+            {'title': 'Mercedes', 'id': 4, 'eng_type': 'преимущественно ДВС'},
         ]
     }})
+
+def TakeCarImagePath(title):
+    dictionary =  {
+        'Ford': 'photo/Ford.jpg',
+        'Tesla': 'photo/Tesla.jpg',
+        'BMW': 'photo/BMW.jpg',
+        'Mercedes': 'photo/Mercedes.jpg',
+    }
+    return dictionary[title]
+
+def TakeCarDescription(title):
+    dictionary =  {
+        'Ford': {
+            'eng_type': 'преимущественно ДВС',
+            'country': 'США'
+            },
+        'Tesla': {
+            'eng_type': 'Электрический',
+            'country': 'США'
+            },
+        'BMW': {
+            'eng_type': 'преимущественно ДВС',
+            'country': 'Германия'
+            },
+        'Mercedes': {
+            'eng_type': 'преимущественно ДВС',
+            'country': 'Германия'
+        },
+    }
+    return dictionary[title]
